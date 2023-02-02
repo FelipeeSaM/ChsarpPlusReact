e9ef23436b7ac11346d0a7620ae9b39df49b514d
eu mesmo <luiz_sena@hotmail.com>
2023-02-02 01:00:19 -0300

    Finalizando cadastro de produtos

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -5,6 +5,7 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { FetchProducts } from './components/FetchProduct';
+import { AddProduct } from './components/AddProduct';
 
 import './custom.css'
 
@@ -18,6 +19,8 @@ export default class App extends Component {
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
         <Route path='/fetch-product' component={FetchProducts} />
+        <Route path='/add-product' component={AddProduct} />
+        <Route path='/product/edit/:id' component={AddProduct} />
       </Layout>
     );
   }

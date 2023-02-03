3ee759e7ca82d31c29a0cf2bb59dc2ebd2b24b09
eu mesmo <luiz_sena@hotmail.com>
2023-02-03 01:37:13 -0300

    iniciando user

---
+++ Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsharpPlusReact.Model {
+    [Table("User")]
+    public class User {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        public string User_name { get; set; }
+
+        [Column("CPF")]
+        public string User_CPF { get; set; }
+
+    }
+}

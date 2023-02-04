4d6981bec9aced706ad0264456d89e60d22c959f
eu mesmo <luiz_sena@hotmail.com>
2023-02-04 02:36:13 -0300

    add: migration user

--- Configuration/Context.cs
+++ Configuration/Context.cs
@@ -13,5 +13,6 @@ namespace CsharpPlusReact.Configuration {
         }
 
         public DbSet<Product> Product { get; set; }
+        public DbSet<User> User { get; set; }
     }
 }
---
+++ Migrations/20230203151604_TesteCricri.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CsharpPlusReact.Migrations
+{
+    public partial class TesteCricri : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
